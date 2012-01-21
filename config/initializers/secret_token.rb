b01dbd16f73397c30e29caf2d3cae446140dfda4
HungryCoder <rajuru@gmail.com>
2012-01-21 15:10:37 +0600

    project initialized with rails

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Rbilling::Application.config.secret_token = '1ee0c52a90efe18419481cc68315e88ac14fd7f97c26516ad957ab31c3e26eb0dcfe7ccb78cfdf25b40c0f620e3dbb768b996608fa8b9f52f2e9917e252b007b'
